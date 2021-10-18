80dd58f6405d4366b850aa61673cdec51e9cae62
23hughk <90779209+23hughk@users.noreply.github.com>
2021-10-18 09:50:23 -0400

    main

--- Assets/Scripts/PuckController2.cs
+++ Assets/Scripts/PuckController2.cs
@@ -6,16 +6,41 @@ public class PuckController2 : MonoBehaviour
 {
     public int[] direction = { 0, 1, 2, 3 };
     public int moveDirection;
+    public float puckSpeed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
         moveDirection = direction[Random.Range(0, 4)];
-        Debug.Log("moveDirection = " + movedirection);
+        Debug.Log("moveDirection = " + moveDirection);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(moveDirection == 0)
+        {
+            transform.Translate(transform.up * puckSpeed * Time.deltaTime);
+        }
+
+        if (moveDirection == 1)
+        {
+            transform.Translate(transform.right * puckSpeed * Time.deltaTime);
+        }
+
+        if (moveDirection == 2)
+        {
+            transform.Translate(transform.up * -puckSpeed * Time.deltaTime);
+        }
+
+        if (moveDirection == 3)
+        {
+            transform.Translate(transform.right * -puckSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collison)
+    {
+        Debug.Log("WALLLLLLLLLLLLLLLLLLL");
+        puckSpeed = -puckSpeed;
     }
 }
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -5,44 +5,46 @@ using UnityEngine;
 public class Testing : MonoBehaviour
 {
     public float speed;
-    public float xrange;
-    public float yrange;
+    public float xRange;
+    public float yRange;
     public GameObject Puck;
+    public GameObject Blocky;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Instantiate(Blocky, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
     }
 
     private void LateUpdate()
     {
-        if (transform.position.x > xrange)
+        if (transform.position.x > xRange)
         {
-            transform.position = new Vector2(xrange, transform.position.y);
+            transform.position = new Vector2(xRange, transform.position.y);
         }
 
-        if (transform.position.x < -xrange)
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector2(-xrange, transform.position.y);
+            transform.position = new Vector2(-xRange, transform.position.y);
         }
 
-        if (transform.position.y > yrange)
+        if (transform.position.y > yRange)
         {
-            transform.position = new Vector2(transform.position.x, yrange);
+            transform.position = new Vector2(transform.position.x, yRange);
         }
 
-        if (transform.position.y < -yrange)
+        if (transform.position.y < -yRange)
         {
-            transform.position = new Vector2(transform.position.x, -yrange);
+            transform.position = new Vector2(transform.position.x, -yRange);
         }
     }
     // Update is called once per frame
     void Update()
     {
-        Instantiate(Puck, new Vector2(Random.Range(-xrange, xrange), Random.Range(-yrange, yrange)), Quaternion.identity);
-
-
+        Instantiate(Puck, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+        GameObject[] puckArray;
+        puckArray = GameObject.FindGameObjectsWithTag("Puck");
+        Debug.Log("Puck Count: " + puckArray.Length);
 
 
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -55,5 +57,14 @@ public class Testing : MonoBehaviour
         transform.Translate(movement * speed * Time.deltaTime);
 
         
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Blocky"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Hit Blocky!");
+        }
     }
 }
